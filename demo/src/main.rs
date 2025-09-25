fafdb873fe3f88e62ef8924241517ab61e7bb6d3
Flora Hill <imflo.pink@gmail.com>
2025-09-25 21:09:11 +1000

    DEMO

---
+++ demo/src/main.rs
@@ -0,0 +1,218 @@
+use {
+    crate::widgets::{MessageList, Textbox},
+    anyhow::{Context, Result},
+    bincode::{Decode, Encode, config::Configuration},
+    crossterm::{
+        event::{self, Event, KeyCode, KeyModifiers},
+        execute,
+        terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
+    },
+    fl_uid::Fluid,
+    flesh::{
+        modes::lora::{Lora, LoraSettings},
+        transport::{PacketTransport, encoding::FLESHMessage, network::Network},
+    },
+    futures::StreamExt,
+    futures_signals::signal::Mutable,
+    itertools::Itertools,
+    ratatui::prelude::*,
+    std::{
+        env,
+        io::{self, Stdout},
+        path::Path,
+        time::Duration,
+    },
+    tokio::{
+        spawn,
+        sync::mpsc::{UnboundedSender, unbounded_channel},
+    },
+};
+
+mod widgets;
+
+#[derive(Debug, Clone, Encode, Decode, PartialEq, Eq)]
+pub enum Message {
+    Text { author: String, content: String },
+    Join(String),
+}
+
+pub struct App {
+    messages: Mutable<Vec<Message>>,
+    send: UnboundedSender<Message>,
+    textbox_state: (String, usize),
+    should_quit: bool,
+    name: String,
+}
+
+impl App {
+    pub async fn new() -> Self {
+        let lora = Lora::new(
+            Path::new(&env::var("LORA").expect("No LoRa env")).to_path_buf(),
+            9600,
+            LoraSettings { spread_factor: 9, frequency_hz: 915_000_000, bandwidth_khz: 10 },
+            false,
+        )
+        .await
+        .expect("Failed to setup LoRa");
+
+        let msgs = Mutable::<Vec<Message>>::new(Vec::new());
+
+        let s = Self {
+            messages: msgs.clone(),
+            send: Self::lora(lora, msgs.clone()),
+            textbox_state: Default::default(),
+            should_quit: false,
+            name: std::env::var("USER").unwrap_or(Fluid::new().to_string()),
+        };
+
+        s.send.send(Message::Join(s.name.to_string())).unwrap();
+        s
+    }
+
+    pub fn lora(lora: Lora, msgs: Mutable<Vec<Message>>) -> UnboundedSender<Message> {
+        let network = Network::new(lora.clone());
+        let (tx, mut rx) = unbounded_channel::<Message>();
+
+        spawn({
+            let msgs = msgs.clone();
+            async move {
+                let msgs = msgs.clone();
+                network
+                    .as_stream()
+                    .filter_map(|m| async move {
+                        bincode::decode_from_slice::<Message, Configuration>(&m.body, Configuration::default())
+                            .map(|v| v.0)
+                            .ok()
+                    })
+                    .for_each({
+                        let msgs = msgs.clone();
+                        move |m| {
+                            let msgs = msgs.clone();
+                            async move {
+                                msgs.clone().set({
+                                    let mut l = msgs.get_cloned();
+                                    l.push(m);
+                                    l
+                                })
+                            }
+                        }
+                    })
+            }
+        });
+
+        spawn(async move {
+            while let Some(msg) = rx.recv().await {
+                let encoded = FLESHMessage::new(flesh::transport::encoding::MessageType::Data { status: 100 })
+                    .with_body(bincode::encode_to_vec::<_, Configuration>(msg.clone(), Configuration::default()).unwrap());
+
+                msgs.clone().set({
+                    let mut l = msgs.get_cloned();
+                    l.push(msg);
+                    l.dedup();
+                    l
+                });
+
+                lora.send(&encoded.serialize().unwrap()).await.unwrap();
+            }
+        });
+
+        tx
+    }
+
+    pub fn tick(&mut self, f: &mut Frame<'_>) -> anyhow::Result<()> {
+        if event::poll(Duration::from_millis(50)).context("event poll failed")? {
+            if let Event::Key(key) = event::read().context("event read failed")? {
+                match key.code {
+                    // Quit
+                    KeyCode::Esc => {
+                        self.should_quit = true;
+                    }
+
+                    // Send
+                    KeyCode::Enter if !self.textbox_state.0.trim().is_empty() => {
+                        self.send
+                            .send(Message::Text { author: self.name.clone(), content: self.textbox_state.0.clone() })?;
+                        self.textbox_state = (String::new(), 0);
+                    }
+
+                    KeyCode::Backspace => {
+                        if self.textbox_state.1 > 0 {
+                            self.textbox_state.0.remove(self.textbox_state.1 - 1);
+                            self.textbox_state.1 -= 1;
+                        }
+                    }
+                    KeyCode::Home | KeyCode::Left if key.modifiers.contains(KeyModifiers::CONTROL) => {
+                        self.textbox_state.1 = 0;
+                    }
+                    KeyCode::End | KeyCode::Right if key.modifiers.contains(KeyModifiers::CONTROL) => {
+                        self.textbox_state.1 = self.textbox_state.0.len();
+                    }
+                    KeyCode::Left => {
+                        if self.textbox_state.1 > 0 {
+                            self.textbox_state.1 -= 1;
+                        }
+                    }
+                    KeyCode::Right => {
+                        if self.textbox_state.1 < self.textbox_state.0.len() {
+                            self.textbox_state.1 += 1;
+                        }
+                    }
+                    KeyCode::Delete => {
+                        self.textbox_state.0.remove(self.textbox_state.1);
+                    }
+                    KeyCode::Char(c) => {
+                        self.textbox_state.0.insert(self.textbox_state.1, c);
+                        self.textbox_state.1 += 1;
+                    }
+
+                    _ => {}
+                }
+            }
+        }
+
+        let binding = Layout::new(Direction::Vertical, [Constraint::Fill(1), Constraint::Length(3)]).split(f.area());
+        let (lay_list, lay_input) = binding.into_iter().collect_tuple().unwrap();
+
+        f.render_widget(MessageList::new(self.messages.get_cloned()), *lay_list);
+        f.render_stateful_widget(Textbox::new("Compose"), *lay_input, &mut self.textbox_state);
+        std::thread::sleep(Duration::from_millis(10));
+
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let mut terminal = setup_terminal().context("setup failed")?;
+    terminal.clear().unwrap();
+    run(&mut terminal, App::new().await).await.context("run failed")?;
+    restore_terminal(&mut terminal).context("restore terminal failed")?;
+    Ok(())
+}
+
+fn setup_terminal() -> Result<Terminal<CrosstermBackend<Stdout>>> {
+    let mut stdout = io::stdout();
+    enable_raw_mode().context("failed to enable raw mode")?;
+    execute!(stdout, EnterAlternateScreen).context("unable to enter alternate screen")?;
+    Terminal::new(CrosstermBackend::new(stdout)).context("creating terminal failed")
+}
+
+fn restore_terminal(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<()> {
+    disable_raw_mode().context("failed to disable raw mode")?;
+    execute!(terminal.backend_mut(), LeaveAlternateScreen).context("unable to switch to main screen")?;
+    terminal.show_cursor().context("unable to show cursor")
+}
+
+async fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>, mut app: App) -> Result<()> {
+    loop {
+        terminal.draw(|f| {
+            app.tick(f).unwrap();
+        })?;
+
+        if app.should_quit {
+            break;
+        }
+    }
+
+    Ok(())
+}
---
+++ demo/src/widgets.rs
@@ -0,0 +1,103 @@
+use {
+    crate::Message,
+    itertools::{Itertools, repeat_n},
+    ratatui::{
+        layout::{Constraint, Direction, Layout, Margin},
+        style::{Color, Style, Stylize},
+        text::{Line, Span},
+        widgets::{Block, Borders, Paragraph, StatefulWidget, Widget},
+    },
+    std::fmt::Display,
+};
+
+pub struct MessageList(Vec<Message>);
+impl MessageList {
+    pub fn new(m: Vec<Message>) -> Self { Self(m) }
+}
+
+const COLOURS: &[Color] =
+    &[Color::LightBlue, Color::LightCyan, Color::LightGreen, Color::LightMagenta, Color::LightRed, Color::LightYellow];
+
+impl Into<Line<'_>> for Message {
+    fn into(self) -> Line<'static> {
+        match self {
+            Message::Text { author, content } => Line::from_iter([
+                Span::from(format!("{author}: "))
+                    .bold()
+                    .fg(COLOURS[author.as_bytes().iter().fold(0usize, |a, b| a as usize + *b as usize) % COLOURS.len()]),
+                Span::from(content).fg(Color::White),
+            ]),
+            Message::Join(author) => Line::from_iter([
+                Span::from(format!("{author} "))
+                    .bold()
+                    .fg(COLOURS[author.as_bytes().iter().fold(0usize, |a, b| a as usize + *b as usize) % COLOURS.len()]),
+                Span::from("joins the room.").fg(Color::Gray),
+            ]),
+        }
+    }
+}
+
+impl Widget for MessageList {
+    fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer)
+    where
+        Self: Sized,
+    {
+        let block = Block::default()
+            .title("Messages")
+            .borders(Borders::ALL)
+            .border_style(ratatui::style::Style::default().fg(ratatui::style::Color::Yellow));
+
+        let messages = self.0.iter().rev().take(area.height.saturating_sub(2).into()).collect_vec();
+        let lay = Layout::new(Direction::Vertical, repeat_n(Constraint::Length(1), messages.len()))
+            .split(area.inner(Margin::new(0, 2)));
+
+        lay.into_iter().zip(messages.into_iter().rev()).for_each(|(a, b)| {
+            Into::<Line>::into(b.clone()).render(*a, buf);
+        });
+
+        block.render(area, buf);
+    }
+}
+
+pub struct Textbox {
+    label: String,
+}
+
+impl Textbox {
+    pub fn new(l: impl Display) -> Self { Self { label: l.to_string() } }
+}
+impl StatefulWidget for Textbox {
+    type State = (String, usize);
+
+    fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer, state: &mut Self::State) {
+        let (value, cursor) = state.clone();
+
+        let mut offset = 0;
+        let slice = match area.columns().count() < value.len() + 3 && cursor > area.columns().count() {
+            false => value.clone(),
+            true => {
+                // slice with the cursor at the end
+                offset = cursor - area.columns().count() + 3;
+                value[offset..].to_string()
+            }
+        };
+
+        let block = Block::default()
+            .title(self.label)
+            .borders(Borders::ALL)
+            .border_style(ratatui::style::Style::default().fg(ratatui::style::Color::Yellow));
+
+        Paragraph::new(add_cursor(slice, cursor - offset)).block(block).render(area, buf);
+    }
+}
+
+pub fn add_cursor<'a>(s: String, c: usize) -> Line<'a> {
+    Line::from(vec![
+        Span::raw(s[..c].to_string()),
+        Span::styled(s.clone().chars().nth(c).unwrap_or(' ').to_string(), Style::new().bg(Color::Yellow)),
+        Span::raw(match c == s.len() {
+            true => String::new(),
+            false => s[c + 1..].to_string(),
+        }),
+    ])
+}
